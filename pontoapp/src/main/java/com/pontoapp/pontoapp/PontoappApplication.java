0275263a7ad55ca191d8a27c7e3c960dabb22beb
Yohan Gregorio <69416757+yohangreg@users.noreply.github.com>
2024-10-30 17:24:56 +0000

    Criação do projeto Spring PontoApp

---
+++ pontoapp/src/main/java/com/pontoapp/pontoapp/PontoappApplication.java
@@ -0,0 +1,13 @@
+package com.pontoapp.pontoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoappApplication.class, args);
+	}
+
+}
---
+++ pontoapp/src/test/java/com/pontoapp/pontoapp/PontoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontoapp.pontoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
