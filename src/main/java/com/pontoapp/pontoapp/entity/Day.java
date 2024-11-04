1a89ee39513a3fa7bd3dacbb4f002ef9b19b9230
Yohan Gregorio <69416757+yohangreg@users.noreply.github.com>
2024-11-04 18:41:39 +0000

    new file: src/dto modified: Day.java

--- src/main/java/com/pontoapp/pontoapp/entity/Day.java
+++ src/main/java/com/pontoapp/pontoapp/entity/Day.java
@@ -19,6 +19,35 @@ public class Day {
     @ManyToOne
     @JoinColoumn(name = "user_id", nullable = false)
     private User user;
- 
-    
+
+    public Day(DayDTO dayDTO) {
+        BeanUtils.copyProperties(dayDTO, this);
+    }
+
+    public Day() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getDot() {
+        return dot;
+    }
+
+    public void setDot(Date dot) {
+        this.dot = dot;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
