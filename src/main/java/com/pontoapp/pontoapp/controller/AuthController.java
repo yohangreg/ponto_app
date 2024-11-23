0edf1853af0cdcb3d8955a9164f7a702bdec2f27
Yohan Gregorio <yohanggg26@gmail.com>
2024-11-23 17:08:10 -0300

    modified:   src/main/java/com/pontoapp/pontoapp/controller/AuthController.java

--- src/main/java/com/pontoapp/pontoapp/controller/AuthController.java
+++ src/main/java/com/pontoapp/pontoapp/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.pontoapp.pontoapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin
 public class AuthController {
    
     @Autowired
